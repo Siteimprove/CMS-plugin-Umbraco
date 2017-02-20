37e1075b8fbd0e94f2734feea1ae5a5deb56acab
Emil Olsson <emil@nmester.dk>
2017-02-20 14:14:30 +0100

    Init

---
+++ EventHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Core;
+using Umbraco.Core.Services;
+using Umbraco.Web.Trees;
+
+namespace SiteImprove.Umbraco.Plugin
+{
+    public class EventHandler : ApplicationEventHandler
+    {
+        protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            TreeControllerBase.MenuRendering += TreeControllerBase_MenuRendering;
+            base.ApplicationStarted(umbracoApplication, applicationContext);
+        }
+
+        private void TreeControllerBase_MenuRendering(TreeControllerBase sender, MenuRenderingEventArgs e)
+        {
+            if(sender.TreeAlias == "content")
+            {
+                e.Menu.Items.Add(new SiteImproveMenuItem());
+            }
+        }
+    }
+}
---
+++ SiteImproveMenuItem.cs
@@ -0,0 +1,14 @@
+﻿using Umbraco.Web.Models.Trees;
+
+namespace SiteImprove.Umbraco.Plugin
+{
+    [ActionMenuItem("SiteImproveMenuActions", "Check")]
+    public class SiteImproveMenuItem : ActionMenuItem
+    {
+        public SiteImproveMenuItem()
+        {
+            this.Name = "Check SiteImprove";
+            this.SeperatorBefore = true;
+        }
+    }
+}
