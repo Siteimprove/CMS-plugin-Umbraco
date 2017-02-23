77a1e69e2b6ac88c0915b74656198907a6faf601
Emil Olsson <emil@nmester.dk>
2017-02-23 10:40:56 +0100

    Package v0.1.2

---
+++ UmbracoPackages/SiteImproveUmbracoPlugin v0.1.2/siteimprove.controller.js
@@ -0,0 +1,24 @@
+﻿
+angular.module('umbraco').factory('SiteImproveMenuActions', ['$http', '$rootScope', '$location', '$routeParams', '$log', 'appState', 'editorState', function ($http, $rootScope, $location, $routeParams, $log, appState, editorState) {
+    var factory = new function () {
+        var _si = window._si || [],
+            siHelper = window.siteimprove.helper;
+
+        if (window.siteimprove.initiated) {
+            console.log('SiteImprove is already initiated');
+        } else {
+            $rootScope.$on('$routeChangeSuccess', siHelper.on$routeChangeSuccess.bind(siHelper));
+        }
+
+        this.Start = function () {
+            siHelper.pushSi('input', editorState.current.urls[0]);
+        };
+
+        this.Recheck = function () {
+            siHelper.pushSi('recheck', editorState.current.urls[0]);
+        };
+
+    };
+
+    return factory;
+}]);
