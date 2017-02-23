9685bf56c11a7f73197234f588a0f93eac71ef83
Emil Olsson <emil@nmester.dk>
2017-02-23 16:23:40 +0100

    Modify the package.xml when running the powershell script

--- createPackage.ps1
+++ createPackage.ps1
@@ -1,25 +1,35 @@
 #Package information
 $packageGuid = "4592a559-2bd8-4e75-b79e-6343764a454c"
 $packageName = "SiteImproveUmbracoPlugin"
-$packageVersion = "0.1.5"
+$packageVersion = "0.2.0"
 
 #Locations
 $currentLocation = Get-Location
 $pluginFolder = "$currentLocation\App_Plugins\SiteImprove"
 $UmbracoPackagesFolder = "$currentLocation\UmbracoPackages"
 
+# Build application
 Write-Host "Start Build application..."
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe SiteImprove.Umbraco.Plugin.csproj /p:Configuration=Debug
 
+# Modify the umbraco package.xml with the settings in this file
+Write-Host "Change package.xml data..."
+$packageXml = [xml](Get-Content "$currentLocation\package.xml")
+$packageXml.SelectSingleNode("//version").InnerText = $packageVersion
+$packageXml.Save("$currentLocation\package.xml")
+
+# Create the package folder
 $newPackageRootFolder = "$UmbracoPackagesFolder\$packageName" + "_v$packageVersion" 
 $newPackageFolder = "$newPackageRootFolder\$packageGuid"
 New-Item $newPackageFolder -type directory
 Write-Host "Created folder $newPackageFolder"
 
+# Move all the items
 Copy-Item "$pluginFolder\*" $newPackageFolder
 Copy-Item "$currentLocation\bin\Debug\SiteImprove.Umbraco.Plugin.dll" $newPackageFolder
 Copy-Item "$currentLocation\package.xml" $newPackageFolder
 
+# Zip the folder, ready to use in Umbraco
 Compress-Archive -Path $newPackageRootFolder -DestinationPath "$newPackageRootFolder.zip"
 
 Write-Host "Done"
