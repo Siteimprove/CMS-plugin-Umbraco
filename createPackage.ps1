d0d0048c42cba0f5e8c164b5f70e651721c68776
Emil Olsson <emilolsson93@gmail.com>
2017-03-02 17:37:08 +0100

    Modify powershell createPackage fiels should have guid names

--- createPackage.ps1
+++ createPackage.ps1
@@ -12,25 +12,45 @@ $UmbracoPackagesFolder = "$currentLocation\UmbracoPackages"
 Write-Host "Start Build application..."
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe SiteImprove.Umbraco.Plugin.csproj /p:Configuration=Debug
 
+# Create the package folder
+$newPackageRootFolder = "$UmbracoPackagesFolder\$packageName" + "_v$packageVersion" 
+$newPackageFolder = "$newPackageRootFolder\$packageGuid"
+Remove-Item -Recurse -Force $newPackageRootFolder
+New-Item $newPackageFolder -type directory -Force
+Write-Host "Created folder $newPackageFolder"
+
 # Modify the umbraco package.xml with the settings in this file
 Write-Host "Change package.xml data..."
 $packageXml = [xml](Get-Content "$currentLocation\package.xml")
 $packageXml.SelectSingleNode("//version").InnerText = $packageVersion
 $packageXml.Save("$currentLocation\package.xml")
 
-# Create the package folder
-$newPackageRootFolder = "$UmbracoPackagesFolder\$packageName" + "_v$packageVersion" 
-$newPackageFolder = "$newPackageRootFolder\$packageGuid"
-New-Item $newPackageFolder -type directory -Force
-Write-Host "Created folder $newPackageFolder"
-
 # Move all the items
 Copy-Item "$pluginFolder\*" $newPackageFolder -Recurse -Force
 Copy-Item "$currentLocation\bin\Debug\SiteImprove.Umbraco.Plugin.dll" $newPackageFolder -Force
 Copy-Item "$currentLocation\package.xml" $newPackageFolder -Force
 
+$newPackageXml = [xml](Get-Content "$newPackageFolder\package.xml")
+
+Get-ChildItem $newPackageFolder\* -Recurse -File |
+	ForEach-Object {
+		# Change all names except package.xml
+		if($_.Name -eq "package.xml") { return }
+
+		$guid = New-Guid
+		$extension = $_.Extension
+		$dir = $_.Directory
+		Rename-Item "$dir\$_" "$dir\$guid$extension"
+		
+		$newPackageXml.SelectSingleNode("//guid[text()='"+ $_.Name +"']").InnerText = "$guid$extension"
+	}
+$newPackageXml.Save("$newPackageFolder\package.xml")
+
 # Zip the folder, ready to use in Umbraco
 Compress-Archive -Path $newPackageRootFolder -DestinationPath "$newPackageRootFolder.zip" -Force
+
+# Cleanup
+Remove-Item -Recurse -Force $newPackageRootFolder
 
 Write-Host "Done"
 Read-Host
