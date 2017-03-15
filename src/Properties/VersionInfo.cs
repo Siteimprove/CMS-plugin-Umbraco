ec3bb503c7c69af7f8eb62bd79c89e25eb80d8fd
Emil Olsson <emilolsson93@gmail.com>
2017-03-15 15:50:19 +0100

    Build v1.3.1

--- src/Properties/VersionInfo.cs
+++ src/Properties/VersionInfo.cs
@@ -13,8 +13,8 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.3.0")]
-[assembly: AssemblyFileVersion("1.3.0.51461")]
-[assembly: AssemblyInformationalVersion("1.3.0")]
+[assembly: AssemblyVersion("1.3.1")]
+[assembly: AssemblyFileVersion("1.3.0.43225")]
+[assembly: AssemblyInformationalVersion("1.3.1")]
 
 
