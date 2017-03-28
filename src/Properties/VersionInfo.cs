73e8de157d9d8ddfb6f756799ce45eb3563a2536
Emil Olsson <emilolsson93@gmail.com>
2017-03-28 15:55:25 +0200

    Add uninstall transformations to nuget package

--- src/Properties/VersionInfo.cs
+++ src/Properties/VersionInfo.cs
@@ -13,8 +13,8 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.3.5")]
-[assembly: AssemblyFileVersion("1.3.0.46623")]
-[assembly: AssemblyInformationalVersion("1.3.5")]
+[assembly: AssemblyVersion("1.3.6")]
+[assembly: AssemblyFileVersion("1.3.0.43445")]
+[assembly: AssemblyInformationalVersion("1.3.6")]
 
 
